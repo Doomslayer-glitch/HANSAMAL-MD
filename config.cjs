442ec6ea97bf76c3b45ad1486e78bb31cb692e0a
Aatrox <timeyinrexotuedon@gmail.com>
2024-12-23 00:41:48 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || ",Imalka-MD&JF54tgcm,"
+  SESSION_ID: process.env.SESSION_ID || ",Imalka-MD&ib9VeJdM,"
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
@@ -18,8 +18,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "private",
-  OWNER_NAME: process.env.OWNER_NAME || "𝔄𝔏𝔈𝔛𝔄𝔑𝔇𝔈ℜ 𝔄𝔑𝔇𝔈𝔎𝔖𝔒𝔑",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "234708 8751140",
+  OWNER_NAME: process.env.OWNER_NAME || "ThePinnacle",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "+917638822171",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
